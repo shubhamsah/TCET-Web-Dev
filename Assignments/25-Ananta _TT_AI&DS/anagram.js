2001e0afe6df4b0f12218fcfc1ac49a5660a14f1
Anantask <118111169+Anantask@users.noreply.github.com>
2024-06-19 12:33:28 +0530

    Update anagram.js

---
+++ Assignments/25-Ananta _TT_AI&DS/anagram.js
@@ -0,0 +1,9 @@
+function isAnagram(str1, str2) {
+    const sortString = (str) => str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().split('').sort().join('');
+    return sortString(str1) === sortString(str2);
+}
+
+// Example usage:
+console.log(isAnagram("listen", "silent")); 
+console.log(isAnagram("hello", "billion"));
+console.log(isAnagram("Dormitory", "Dirty room")); 
