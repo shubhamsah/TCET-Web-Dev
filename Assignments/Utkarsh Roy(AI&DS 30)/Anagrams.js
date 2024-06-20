5087ed939e1940e941c6eb6b8c32fc04222eb732
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-06-20 10:27:01 +0530

    Delete Assignments/Utkarsh Roy(AI&DS 30)/Anagrams.js

--- Assignments/Utkarsh Roy(AI&DS 30)/Anagrams.js
+++
@@ -1,8 +0,0 @@
-function checkAnagrams(text1, text2) {
-    const normalize = str => str.toLowerCase().split('').sort().join('');
-    return normalize(text1) === normalize(text2);
-}
-
-console.log(checkAnagrams('hello', 'how')); 
-console.log(checkAnagrams('evil', 'vile'));     
-console.log(checkAnagrams('fluster', 'restful'));
